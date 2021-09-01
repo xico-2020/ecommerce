fd50f7b655385076c6a89f26079f790a7266c328
Francisco Paulo <fpaulocgd@outlook.pt>
2021-09-01 23:08:27 +0100

    Paginacao

--- site.php
+++ site.php
@@ -24,13 +24,28 @@ $app->get('/', function() {   // criacao de rota "/"
 
 $app->get("/categories/:idcategory", function($idcategory) {
 
+	$page = (isset($_GET['page'])) ? (int)$_GET['page'] : 1;  // se trouxer num de pagina é esse numero senão é pag 1.
+
 	$category = new Category();
 	$category->get((int)$idcategory);
+
+	$pagination = $category->getProductsPage($page);
+
+	$pages = [];
+
+	for ($i = 1; $i <= $pagination['pages']; $i++) {     // pages vem do return de category.php rota para paginacao
+		array_push($pages, [  // a seguir carrrega os valores necessários para o loop de category.html
+			"link"=>"/categories/".$category->getidcategory()."?page=".$i,  // envia para /categories/ o id da categoria  e concatena com a page (vem da linha $_GET acima) e com a variavel de incremento
+			"page"=>$i
+		]);
+	}
+
 	$page = new Page();  // criar $page que recebe o construtor vazio. Chama o construct e adiciona o header no ecran.
 
 	$page->setTpl("category", [
 		"category"=>$category->getValues(),
-		"products"=>Product::checkList($category->getProducts())
+		"products"=>$pagination["data"],
+		"pages"=>$pages   // pages vem de category.html para passagem de valores e é definido acima.
 	]);  
 
 });
